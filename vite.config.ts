cf8c212fc315527fdb8445bf73c4973a1f34b2ed
neqkuny <crystalizedgw01@gmail.com>
2025-12-10 06:28:01 +0000

    Fix base path in vite config to match basename

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import { viteSingleFile } from "vite-plugin-singlefile";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: './',
+  base: mode === 'production' ? '/secure-password-hub/' : '/',
   server: {
     host: "::",
     port: 8080,
